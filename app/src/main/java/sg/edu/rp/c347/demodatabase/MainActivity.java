986b05dd3c69b18fd49b3842c764b70194d2234e
15017612 <15017612@myrp.edu.sg>
2017-05-12 12:42:11 +0800

    retrieve from database

--- app/src/main/java/sg/edu/rp/c347/demodatabase/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c347/demodatabase/MainActivity.java
@@ -2,12 +2,67 @@ package sg.edu.rp.c347.demodatabase;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnInsert, btnGetTask;
+    TextView tvResults;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DBHelper db = new DBHelper(this);
+        db.getWritableDatabase();
+        db.close();
+
+        btnInsert = (Button) findViewById(R.id.btnInsert);
+        btnGetTask = (Button)findViewById(R.id.btnGetTasks);
+        tvResults = (TextView)findViewById(R.id.tvResults);
+
+        btnInsert.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                // Create the DBHelper object, passing in the
+                // activity's Context
+                DBHelper db = new DBHelper(MainActivity.this);
+
+                // Insert a task
+                db.insertTask("Submit RJ", "25 Apr 2016");
+                db.close();
+            }
+        });
+        btnGetTask.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                // Create the DBHelper object, passing in the
+                // activity's Context
+                DBHelper db = new DBHelper(MainActivity.this);
+
+                // Insert a task
+                ArrayList<String> data = db.getTaskContent();
+                db.close();
+
+                String txt = "";
+                for (int i = 0; i < data.size(); i++) {
+                    Log.d("Database Content", i +". "+data.get(i));
+                    txt += i + ". " + data.get(i) + "\n";
+                }
+                tvResults.setText(txt);
+            }
+        });
+
+
+
     }
+
 }
---
+++ app/src/main/java/sg/edu/rp/c347/demodatabase/Task.java
@@ -0,0 +1,8 @@
+package sg.edu.rp.c347.demodatabase;
+
+/**
+ * Created by 15017612 on 12/5/2017.
+ */
+
+public class Task {
+}
